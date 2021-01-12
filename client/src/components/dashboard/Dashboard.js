12eb69710c59afc094b6ee21b693283b718be49b
Ryan Hines <ryanchines@gmail.com>
2021-01-11 18:12:52 -0600

    added navbar with logout button

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -26,19 +26,6 @@ class Dashboard extends Component {
                                 <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
                             </p>
                         </h4>
-
-                        <button
-                            style={{
-                                    width: "150px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px",
-                                    marginTop: "1rem"
-                                }}
-                            onClick={this.onLogoutClick}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                        >
-                        Logout
-                        </button>
                             
                         <div className="col s6">
                             <Link
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,73 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div className="navbar-fixed">
-                <nav className="z-depth-0">
-                    <div className="nav-wrapper white">
-                        <Link
-                            to="/"
-                            style={{
-                                fontFamily: "monospace"
-                            }}
-                            className="col s5 brand-logo center black-text"
-                        >
-                            <i className="material-icons">code</i>
-                            MERN
-                        </Link>
-                    </div>
-                </nav>
-            </div>
-        )
+import Appbar from '@material-ui/core/Appbar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CodeIcon from '@material-ui/icons/Code';
+import { Button } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logoutUser } from "../../actions/authActions";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flex: 1
+    },
+    icon: {
+        marginRight: theme.spacing(2)
     }
+}));
+
+function Navbar(props) {
+    // render() {
+    //     return (
+    //         <div className="navbar-fixed">
+    //             <nav className="z-depth-0">
+    //                 <div className="nav-wrapper white">
+    //                     <Link
+    //                         to="/"
+    //                         style={{
+    //                             fontFamily: "monospace"
+    //                         }}
+    //                         className="col s5 brand-logo center black-text"
+    //                     >
+    //                         <i className="material-icons">code</i>
+    //                         MERN
+    //                     </Link>
+    //                 </div>
+    //             </nav>
+    //         </div>
+    //     )
+    // }
+    const onLogoutClick = e => {
+        e.preventDefault();
+        props.logoutUser();
+    };
+
+    const classes = useStyles();
+    return (
+        <Appbar position='static' color='secondary'>
+            <Toolbar>
+                    <CodeIcon fontSize='large' className={classes.icon} />
+                    <Typography variant='h6' className={classes.root} >
+                        MERN Message Application
+                    </Typography>
+                { props.isAuthenticated && <Button variant='contained' onClick={onLogoutClick}>Logout</Button> }
+            </Toolbar>
+        </Appbar>
+
+    );
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
