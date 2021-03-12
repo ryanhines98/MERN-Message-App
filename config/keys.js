6888c7dccba383a43627fe68cbfa19444d293070
ryanhines98 <41522676+ryanhines98@users.noreply.github.com>
2021-03-12 16:53:25 -0600

    Keys Removed for Public View

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     // uri needed to connect to the MongoDB
-    mongoURI: "mongodb+srv://admin:7913@cluster0.f9rj7.mongodb.net/MERN?retryWrites=true&w=majority",
+    mongoURI: "",
     // private key for the decoding of requests
-    secretOrKey: "secret"
+    secretOrKey: ""
 };
