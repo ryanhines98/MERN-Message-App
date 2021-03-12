f4d2fb652c6b59825483e63546f46b3a4f24f213
Ryan Hines <ryanchines@gmail.com>
2021-03-12 16:51:10 -0600

    deleted read_me for create-react-app

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+/*
+    holds the keys used in other files
+*/
+
+module.exports = {
+    // uri needed to connect to the MongoDB
+    mongoURI: "mongodb+srv://admin:7913@cluster0.f9rj7.mongodb.net/MERN?retryWrites=true&w=majority",
+    // private key for the decoding of requests
+    secretOrKey: "secret"
+};
